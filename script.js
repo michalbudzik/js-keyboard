bd854f97d4110148d8b8e1580efb424dc692e78e
michalbudzikl <michal@mindborn.pl>
2023-02-07 21:40:32 +0100

    Add Comments

--- script.js
+++ script.js
@@ -2,14 +2,18 @@ const containerKeys = document.getElementById('container-keys');
 const keys = containerKeys.querySelectorAll('button');
 
 document.addEventListener('keydown', (e) => {
+  // Stop if event is repeating so it triggers piano key press only once
   if (e.repeat) return;
+  // Search for button with value same as key pressed
   keys.forEach(button => {
     if (button.value.toLowerCase() === e.key) {
+      // Trigger focus on selected button
       button.focus();
     }
   });
 });
 
+// Reset current active DOM element when key is up
 document.addEventListener('keyup', () => {
   document.activeElement.blur();
 });
